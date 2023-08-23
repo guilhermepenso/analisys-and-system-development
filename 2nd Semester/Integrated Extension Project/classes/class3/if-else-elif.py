61b0b35364b88225be94a6b99ca514826796df5b
Guilherme Penso <109317629+guilhermepenso@users.noreply.github.com>
2023-08-23 20:24:58 -0300

    if-else-elif

---
+++ 2nd Semester/Integrated Extension Project/classes/class3/if-else-elif.py
@@ -0,0 +1,8 @@
+n1 = int(input("Digite o 1° Número: "))
+n2 = int(input("Digite o 2° Número: "))
+if n1 > n2:
+    print ("n1 > n2")
+elif n1 < n2:
+    print ("n1 < n2")
+else:
+    print ("n1 == n2")
---
+++ 2nd Semester/Integrated Extension Project/classes/class3/true-false.py
@@ -0,0 +1,15 @@
+# leia dois numeros via teclado e imprima os resultados para:
+# 1. num01 == num02
+# 2. num01 + num02 > num01 / 2
+# 3. num01 != num02
+# 4. num01 >= 0
+# 5. num01 >= -1
+
+num01 = int (input("Digite o 1° Número: "))
+num02 = int (input("Digite o 2° Número: "))
+
+print (num01 == num02)
+print (num01 + num02 > num01 / 2)
+print (num01 != num02)
+print (num01 >= 0)
+print (num01 >= -1)
