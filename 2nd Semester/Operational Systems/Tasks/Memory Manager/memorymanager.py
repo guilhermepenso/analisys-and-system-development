3c346f75ab3393bc80a1585d108d04032522149b
Guilherme Penso <109317629+guilhermepenso@users.noreply.github.com>
2023-08-27 12:38:17 -0300

    env error

--- 2nd Semester/Operational Systems/Tasks/Memory Manager/memorymanager.py
+++ 2nd Semester/Operational Systems/Tasks/Memory Manager/memorymanager.py
@@ -1,12 +1,12 @@
 from PyQt5 import QtWidgets
-from PyQt5.QtWidgets import QApplication,QMainWindows
+from PyQt5.QtWidgets import QApplication,QMainWindow
 import sys
 
 def demo():
     app=QApplication(sys.argv)
     win=QMainWindow()
     win.setGeometry(300,300,300,300)
-    win.setWindowTittle("Demo app")
+    win.setWindowTitle("Demo app")
     label= QtWidgets.QLabel(win)
     label.setText("Kyosune")
     label.move(100,100)
