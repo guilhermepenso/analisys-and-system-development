ae522faab80877b66333e1096176d816434001fb
Guilherme Penso <109317629+guilhermepenso@users.noreply.github.com>
2023-08-27 18:07:35 -0300

    pyqt5 tests

--- 2nd Semester/Operational Systems/Tasks/Memory Manager/memorymanager.py
+++ 2nd Semester/Operational Systems/Tasks/Memory Manager/memorymanager.py
@@ -2,16 +2,37 @@ from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QApplication,QMainWindow
 import sys
 
-def demo():
-    app=QApplication(sys.argv)
-    win=QMainWindow()
-    win.setGeometry(300,300,300,300)
-    win.setWindowTitle("Demo app")
-    label= QtWidgets.QLabel(win)
-    label.setText("Kyosune")
-    label.move(100,100)
+class MyWindow(QMainWindow):
+    def __init__(self):
+        super(MyWindow, self).__init__()
+        self.setGeometry(1000,200,300,300)
+        self.setWindowTitle("Test Window")
+        self.initUI()
     
+    def initUI(self):
+        self.label = QtWidgets.QLabel(self)
+        self.label.setText("My first label")
+        self.label.move(100,100)
+        
+        self.b1 = QtWidgets.QPushButton(self)
+        self.b1.setText("Click me")
+        self.b1.clicked.connect(self.clicked)   
+        
+    def clicked(self):
+        self.label.setText("You pressed the button")
+        self.update()
+        
+    def update(self):
+        self.label.adjustSize()
+        
+
+def clicked():
+    print ("clicked")
+
+def window():
+    app=QApplication(sys.argv)
+    win=MyWindow()
     win.show()
     sys.exit(app.exec_())
     
-demo() 
+window() 
