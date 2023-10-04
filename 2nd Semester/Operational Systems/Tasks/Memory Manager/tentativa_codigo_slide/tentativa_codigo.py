a75a5137a90cf4dfa4046144f37468f1f90c7a4e
Guilherme Penso <109317629+guilhermepenso@users.noreply.github.com>
2023-10-04 19:18:54 -0300

    att OS

--- 2nd Semester/Operational Systems/Tasks/Memory Manager/tentativa_codigo_slide/tentativa_codigo.py
+++
@@ -1,9 +0,0 @@
-# bitmap de 100 espaços
-# cada bit pode ser= 0: livre / 1: ocupado
-# lista encadeada contendo as memórias livres e alocadas
-
-# 0: hole
-# 1: process
-
-# posição inicial do processo na lista encadeada
-# largura ou tamanho do processo
