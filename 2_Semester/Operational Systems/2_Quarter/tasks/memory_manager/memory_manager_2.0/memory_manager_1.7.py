3350a92e4bf2043a2d581d5935f25afe94bd8b33
Guilherme Penso <109317629+guilhermepenso@users.noreply.github.com>
2023-10-29 23:11:25 -0300

    device drivers exercise - OP

--- 2_Semester/Operational Systems/2_Quarter/tasks/memory_manager/memory_manager_2.0/memory_manager_1.7.py
+++ 2_Semester/Operational Systems/2_Quarter/tasks/memory_manager/memory_manager_2.0/memory_manager_1.7.py
@@ -5,9 +5,7 @@ import itertools
 import random
 import string
 import time
-import numpy
 
-# Gerador de Letras para Nomear os Processos
 def process_name_generator():
     size = 1
     while True:
@@ -16,7 +14,7 @@ def process_name_generator():
         size += 1
 
 class memory_manager:
-    # Declaração Self / Master para visual no Tkinter
+    
     def __init__(self, master):
         self.master = master
         self.grid = []
@@ -24,8 +22,7 @@ class memory_manager:
         self.chars = process_name_generator()
         self.status = {}
         self.create_grid()
-
-    # Criação da Grade com Linhas e Colunas, definição da Cor do Fundo de cada Slot
+        
     def create_grid(self):
         frame = tk.Frame(self.master)
         frame.pack(expand=True)
@@ -37,18 +34,14 @@ class memory_manager:
                 row.append(label)
                 self.status[(i,j)] = 0
             self.grid.append(row)
-
-    # Função de alocar memória
+            
     def allocate(self):
         n = tkinter.simpledialog.askinteger("Alocar", "Digite o Tamanho do Processo")
         free_blocks = []
         best_fit = None
         total_free = 0
-
-        # Pesquisa dentro da grid
         for i in range(10):
             for j in range(10):
-                # Verificação dos blocos vazios, se verdadeiro aumenta os blocos livres totais e os blocos livres
                 if self.status[(i,j)] == 0:
                     total_free += 1
                     free_blocks.append((i, j))
@@ -57,27 +50,17 @@ class memory_manager:
                         if best_fit is None or len(best_fit) > len(free_blocks):
                             best_fit = list(free_blocks)
                         free_blocks = []
-
-        # Quando terminar de verificar todas as grids, terá outro processo de verificação se o valor digitado é maior que os blocos disponíveis 
         if len(free_blocks) >= n:
             if best_fit is None or len(best_fit) > len(free_blocks):
                 best_fit = list(free_blocks)
-
-        # Se o valor digitado for maior que o total livre, uma mensagem de erro aparecerá 
         if total_free < n:
             messagebox.showinfo("Erro", "Sem Espaço Total")
             return
-
-        # Se o a lista de blocos disponíveis nos free_blocks for menor, uma mensagem aparecerá que não tem espaço sequencial 
         if best_fit is None:
             messagebox.showinfo("Erro", "Sem Espaço Sequencial")
             return
-
-        # Gerador de cores e letrar para cada processo 
         block_color = "#{:06x}".format(random.randint(0x0000, 0xFFFFFF))
         block_name = next(self.chars)
-
-        # Separação de blocos de processos com os nomes e cores 
         for k in range(n):
             self.grid[best_fit[k][0]][best_fit[k][1]]["text"] = block_name
             self.grid[best_fit[k][0]][best_fit[k][1]]["background"] = block_color
@@ -101,58 +84,31 @@ class memory_manager:
                     self.grid[i][j]["text"] = ""
                     self.status[(i,j)] = 0
 
-    # Função de realocação na memória 
     def reallocate(self):
-        block_groups = []
         memory_blocks = []
-        actual_group = ""
-        previous_memory_block = 0
-
-        # Procura dentro da grid dos processos
         for i in range(10):
             for j in range(10):
-                
-                # Verificação de processos que estão ocupados
-                if self.status[(i,j)] != 0 and previous_memory_block == 0:
-                    actual_group = self.grid[i][j]["text"]
-                while actual_group == self.grid[i][j]["text"]:
-                    block_groups.append((i, j, self.grid[i][j]["text"], self.grid[i][j]["background"], self.status[(i,j)]))           
-                        
-                    # Salva os processos ocupadas no memory_blocks 
-                    memory_blocks.append((block_groups))
-                    
-                    # Altera os dados atuais para não ocupados visualmente
+                if self.status[(i,j)] != 0:
+                    memory_blocks.append((i, j, self.grid[i][j]["text"], self.grid[i][j]["background"], self.status[(i,j)]))
                     self.grid[i][j]['background'] = "white"
                     self.grid[i][j]['text'] = ""
                     self.status[(i,j)] = 0
-                block_groups = []
-            previous_memory_block = self.status[(i,j)]
-        # Declaração de variáveis para coordenadas de realocação
         index = 0
         x, y = 0, 0
-        
-        np_memory_blocks = np.array(memory_blocks)
-
-        # Reshape the array into a 2D matrix
-        np_memory_blocks = np_memory_blocks.reshape(-1, 5)
-
-        # Update the grid with the new memory blocks
         for i in range(10):
             for j in range(10):
-                if index < len(np_memory_blocks):
+                if index < len(memory_blocks):
                     if y == 10:
                         x += 1
                         y = 0
                     self.grid[x][y].grid(row=i, column=j)
-                    self.grid[x][y]['text'] = np_memory_blocks[index][2]
-                    self.grid[x][y]['background'] = np_memory_blocks[index][3]
-                    self.status[(i,j)] = np_memory_blocks[index][4]
+                    self.grid[x][y]['text'] = memory_blocks[index][2]
+                    self.grid[x][y]['background'] = memory_blocks[index][3]
+                    self.status[(i,j)] = memory_blocks[index][4]
                     index += 1
-
                 root.update()
                 time.sleep(0.05)
                 y += 1
-
 root = tk.Tk()
 width = 800
 height = 600
@@ -163,7 +119,6 @@ screen_height = root.winfo_screenheight()
 x = (screen_width/2) - (width/2)
 y = (screen_height/2) - (height/2)
 root.geometry('%dx%d+%d+%d' % (width, height, x, y))
-
 mm = memory_manager(root)
 button_frame = tk.Frame(root)
 button_frame.pack(side="top", fill="x", pady=20)
@@ -175,5 +130,4 @@ full_deallocate_button = tk.Button(button_frame, text="Limpar Processos", comman
 full_deallocate_button.pack(side='left', padx=5)
 reallocate_button = tk.Button(button_frame, text="Realocar", command=mm.reallocate, height=5, width=26,  bg='gray', borderwidth=0)
 reallocate_button.pack(side="left", padx=5)
-
 root.mainloop()
