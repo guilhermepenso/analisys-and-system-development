848a27d09ce0a055f6a8abca2c1d15c4018f957b
Guilherme Penso <109317629+guilhermepenso@users.noreply.github.com>
2023-11-01 06:12:59 -0300

    trying some solutions on sorter groups names

--- 2_Semester/Integrated Extension Project/2_Quarter/tasks/sorteador/main.py
+++ 2_Semester/Integrated Extension Project/2_Quarter/tasks/sorteador/main.py
@@ -31,7 +31,39 @@ def sorteio():
 def gerar_grupos(nomes, n):
     grupos = [nomes[i::n] for i in range(n)]
     return grupos
-    
+
+# usar pandas
+#dicionário, usar um loop para separar os nomes com um random de 0 a quantidade de grupos
+# colocar dentro de um dataframe como por exemplo:
+# df = pd.DataFrame({'store': ['B', 'B', 'A', 'A', 'B', 'B', 'A', 'A'],
+                   # 'sales': [12, 25, 8, 14, 10, 20, 30, 30]})
+
+# usar o group by?
+# group by store and sort by sales values in descending order
+#df.sort_values(['store','sales'],ascending=False).groupby('store').head()
+
+#   store	sales
+#1	B	    25
+#5	B	    20
+#0	B	    12
+#4	B	    10
+#6	A	    30
+#7	A	    30
+#3	A	    14
+#2	A	    8
+
+# group by store and sort by sales values in ascending order
+#df.sort_values(['store','sales']).groupby('store').head()
+
+#	store	sales
+#2	A	    8
+#3	A	    14
+#6	A	    30
+#7	A	    30
+#4	B	    10
+#0	B	    12
+#5	B	    20
+#1	B	    25
 
 if __name__ == "__main__":
     app.run(debug=True)
