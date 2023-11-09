c9aa912cbaa47cc6b9cf9dc4010453fafba70756
Guilherme Penso <109317629+guilhermepenso@users.noreply.github.com>
2023-11-09 20:48:04 -0300

    update_memory_manager_2.0

--- 2_Semester/Operational Systems/2_Quarter/tasks/memory_manager/memory_manager_2.0/tempCodeRunnerFile.py
+++
@@ -1,7 +0,0 @@
-
-            if self.status[pos] == 0 and tamanho_memoria > 0:
-                self.grade[pos]["text"] = texto_memoria
-                self.grade[pos]["background"] = self.grupos[texto_memoria]
-                self.status[pos] = 1
-                tamanho_memoria -= 1
-        return self.grade
